4f937441b91e83e9476fee4190e597283ae76437
Christian Fei <crifei93@gmail.com>
2019-12-07 01:20:30 +0100

    init memory of 10 bytes initially all set to zero

--- index.test.js
+++ index.test.js
@@ -4,7 +4,9 @@ const assert = require('assert')
 
 const result = brainfuck('')
 assert(result, 'result is defined')
+assert.deepStrictEqual(result, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'empty memory initialized with 10 bytes')
 
-function brainfuck (commandsString = '') {
-  return []
+function brainfuck (program = '') {
+  const memory = Array(10).fill(0)
+  return memory
 }
