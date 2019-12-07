eb1f2721c95f2374cbd1bdc7ef5df05cbde21317
Christian Fei <crifei93@gmail.com>
2019-12-07 10:56:20 +0100

    hello world test

--- index.test.js
+++ index.test.js
@@ -6,6 +6,7 @@ assert.ok(brainfuck(''), 'result is defined')
 assert.deepStrictEqual(brainfuck(''), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
@@ -14,6 +15,7 @@ assert.deepStrictEqual(brainfuck(''), {
 assert.deepStrictEqual(brainfuck('>'), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 1,
   output: '',
   input: ''
@@ -21,6 +23,7 @@ assert.deepStrictEqual(brainfuck('>'), {
 assert.deepStrictEqual(brainfuck('>>>>>>>>>>>'), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 10,
   output: '',
   input: ''
@@ -29,12 +32,14 @@ assert.deepStrictEqual(brainfuck('>>>>>>>>>>>'), {
 assert.deepStrictEqual(brainfuck('<', {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 1,
   output: '',
   input: ''
 }), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
@@ -43,6 +48,7 @@ assert.deepStrictEqual(brainfuck('<', {
 assert.deepStrictEqual(brainfuck('<'), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
@@ -51,6 +57,7 @@ assert.deepStrictEqual(brainfuck('<'), {
 assert.deepStrictEqual(brainfuck('+'), {
   memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
@@ -59,12 +66,14 @@ assert.deepStrictEqual(brainfuck('+'), {
 assert.deepStrictEqual(brainfuck('-', {
   memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
 }), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
@@ -73,12 +82,14 @@ assert.deepStrictEqual(brainfuck('-', {
 assert.deepStrictEqual(brainfuck('.', {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
 }), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '\u0000',
   input: ''
@@ -87,20 +98,96 @@ assert.deepStrictEqual(brainfuck('.', {
 assert.deepStrictEqual(brainfuck(',', {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: '\u0001'
 }), {
   memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   loops: [],
+  looping: false,
   pointer: 0,
   output: '',
   input: ''
 }, 'input a byte and store it in the byte at the pointer')
 
-function brainfuck (commands = '', { memory = Array(10).fill(0), loops = [], pointer = 0, output = '', input = '' } = {}) {
+assert.deepStrictEqual(brainfuck('[', {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: false,
+  pointer: 0,
+  output: '',
+  input: ''
+}), {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: true,
+  pointer: 0,
+  output: '',
+  input: ''
+}, 'jump forward past the matching ] if the byte at the pointer is zero')
+
+assert.deepStrictEqual(brainfuck(']', {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: true,
+  pointer: 0,
+  output: '',
+  input: ''
+}), {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: false,
+  pointer: 0,
+  output: '',
+  input: ''
+}, 'ends loops with matching ]')
+
+assert.deepStrictEqual(brainfuck('[', {
+  memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: false,
+  pointer: 0,
+  output: '',
+  input: ''
+}), {
+  memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [0],
+  looping: false,
+  pointer: 0,
+  output: '',
+  input: ''
+}, 'pushes commandIndex in loop stack')
+
+assert.deepStrictEqual(brainfuck('>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.', {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: false,
+  pointer: 0,
+  output: '',
+  input: ''
+}), {
+  memory: [100, 87, 33, 0, 0, 0, 0, 0, 0, 0],
+  loops: [],
+  looping: false,
+  pointer: 2,
+  output: 'Hello, World!',
+  input: ''
+}, 'compiles "Hello, world!" program')
+
+function brainfuck (commands = '', { memory = Array(10).fill(0), loops = [], looping = false, pointer = 0, output = '', input = '' } = {}) {
+  let innerLoops = 0
   for (let commandIndex = 0; commandIndex < commands.length; commandIndex++) {
     const command = commands[commandIndex]
+    if (looping) {
+      if (command === '[') innerLoops++
+      if (command === ']') {
+        if (innerLoops === 0) looping = false
+        else innerLoops--
+      }
+      continue
+    }
+
     if (command === '>' && pointer < 10) pointer++
     if (command === '<' && pointer > 0) pointer--
     if (command === '+') memory[pointer]++
@@ -110,6 +197,20 @@ function brainfuck (commands = '', { memory = Array(10).fill(0), loops = [], poi
       memory[pointer] = input.slice(0, 1).charCodeAt()
       input = input.slice(1)
     }
+    if (command === '[') {
+      if (memory[pointer] === 0) {
+        looping = true
+      } else {
+        loops.push(commandIndex)
+      }
+    }
+    if (command === ']') {
+      if (memory[pointer] !== 0) {
+        commandIndex = loops[loops.length - 1]
+      } else {
+        loops.pop()
+      }
+    }
   }
-  return { memory, loops, pointer, output, input }
+  return { memory, loops, looping, pointer, output, input }
 }
