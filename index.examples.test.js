2585c8be382d7315dab9b380378808d2692066b0
Christian Fei <crifei93@gmail.com>
2019-12-07 16:37:36 +0100

    separate test with examples and state

---
+++ index.examples.test.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+const assert = require('assert')
+
+const brainfuck = require('.')
+
+assert.deepStrictEqual(brainfuck.compile('>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.'), {
+  commandIndex: 164,
+  innerLoops: 0,
+  memory: [100, 87, 33, 0],
+  loops: [],
+  looping: false,
+  pointer: 2,
+  output: 'Hello, World!',
+  input: ''
+}, 'compiles "Hello, World!" program')
+
+assert.deepStrictEqual(
+  brainfuck.run('>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.'),
+  'Hello, World!',
+  'runs "Hello, World!" program'
+)
+
+assert.deepStrictEqual(
+  brainfuck.run('++++++++[>++++[>++>+++>+++>+<<<<-]>+>->+>>+[<]<-]>>.>>---.+++++++..+++.>.<<-.>.+++.------.--------.>+.'),
+  'Hello World!',
+  'runs "Hello World!" program'
+)
+
+assert.deepStrictEqual(
+  brainfuck.run(`>+++++++++[<++++++>-]<...`),
+  '666',
+  'complex example - 666'
+)
+
+assert.deepStrictEqual(
+  brainfuck.run(`+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.`, { input: '' }),
+  'a',
+  'complex example - print a'
+)
+
+assert.deepStrictEqual(
+  brainfuck.run(`,[.[-],]`, { input: 'hello' }),
+  'hello',
+  'complex example - pipe input to output'
+)
+
+assert.deepStrictEqual(
+  brainfuck.run(`,[---------[-[++++++++++.[-]]],]`, { input: 'hello\t world\n\t \n\t\t\txoxo' }),
+  'hello world xoxo',
+  'complex example - strip tabs and linefeeds'
+)
+
+assert.deepStrictEqual(
+  brainfuck.run(
+    brainfuck.run(`+++++[>+++++++++<-],[[>--.++>+<<-]>+.->[<.>-]<<,]`, {
+      input: 'hello world'
+    })
+  ),
+  'hello world',
+  'complex example - translate text to brainfuck that prints it'
+)
+
+// assert.deepStrictEqual(
+//   brainfuck.run(`>,[>>>++++++++[<[<++>-]<+[>+<-]<-[-[-<]>]>[-<]<,>>>-]<.[-]<<]`, { input: '0110100001101001' }),
+//   'hi',
+//   'complex example - binary to ascii'
+// )
+
+// assert.deepStrictEqual(
+//   brainfuck.run(`
+//                                 >
+//                                + +
+//                               +   +
+//                              [ < + +
+//                             +       +
+//                            + +     + +
+//                           >   -   ]   >
+//                          + + + + + + + +
+//                         [               >
+//                        + +             + +
+//                       <   -           ]   >
+//                      > + + >         > > + >
+//                     >       >       +       <
+//                    < <     < <     < <     < <
+//                   <   [   -   [   -   >   +   <
+//                  ] > [ - < + > > > . < < ] > > >
+//                 [                               [
+//                - >                             + +
+//               +   +                           +   +
+//              + + [ >                         + + + +
+//             <       -                       ]       >
+//            . <     < [                     - >     + <
+//           ]   +   >   [                   -   >   +   +
+//          + + + + + + + +                 < < + > ] > . [
+//         -               ]               >               ]
+//        ] +             < <             < [             - [
+//       -   >           +   <           ]   +           >   [
+//      - < + >         > > - [         - > + <         ] + + >
+//     [       -       <       -       >       ]       <       <
+//    < ]     < <     < <     ] +     + +     + +     + +     + +
+//   +   .   +   +   +   .   [   -   ]   <   ]   +   +   +   +   +
+//   `),
+//   '',
+//   'complex example - tree'
+// )
