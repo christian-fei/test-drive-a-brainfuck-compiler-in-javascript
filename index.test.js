b5cae0af563bb1f2c15e10e50aaf594661206669
Christian Fei <crifei93@gmail.com>
2019-12-07 01:21:21 +0100

    return object with memory prop

--- index.test.js
+++ index.test.js
@@ -3,10 +3,12 @@
 const assert = require('assert')
 
 const result = brainfuck('')
-assert(result, 'result is defined')
-assert.deepStrictEqual(result, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'empty memory initialized with 10 bytes')
+assert.ok(result, 'result is defined')
+assert.deepStrictEqual(result, {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+}, 'empty memory initialized with 10 bytes')
 
 function brainfuck (program = '') {
   const memory = Array(10).fill(0)
-  return memory
+  return { memory }
 }
