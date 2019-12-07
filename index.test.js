c1f04765843707723b7a13a776d60161726da6c2
Christian Fei <crifei93@gmail.com>
2019-12-07 11:09:47 +0100

    move brainfuck to top

--- index.test.js
+++ index.test.js
@@ -2,6 +2,44 @@
 
 const assert = require('assert')
 
+function brainfuck (commands = '', { memory = [], loops = [], looping = false, innerLoops = 0, pointer = 0, output = '', input = '' } = {}) {
+  for (let commandIndex = 0; commandIndex < commands.length; commandIndex++) {
+    const command = commands[commandIndex]
+    if (looping) {
+      if (command === '[') innerLoops++
+      if (command === ']') {
+        if (innerLoops === 0) looping = false
+        else innerLoops--
+      }
+    }
+
+    if (command === '>' && pointer < 10) pointer++
+    if (command === '<' && pointer > 0) pointer--
+    if (command === '+') memory[pointer] ? memory[pointer]++ : memory[pointer] = 1
+    if (command === '-') memory[pointer] ? memory[pointer]-- : memory[pointer] = 0
+    if (command === '.') output += String.fromCharCode(memory[pointer])
+    if (command === ',' && input.length > 0) {
+      memory[pointer] = input.slice(0, 1).charCodeAt()
+      input = input.slice(1)
+    }
+    if (command === '[') {
+      if (memory[pointer] === 0) {
+        looping = true
+      } else {
+        loops.push(commandIndex)
+      }
+    }
+    if (command === ']') {
+      if (memory[pointer] === 0) {
+        loops.pop()
+      } else {
+        commandIndex = loops[loops.length - 1]
+      }
+    }
+  }
+  return { memory, loops, looping, pointer, output, input }
+}
+
 assert.ok(brainfuck(''), 'result is defined')
 assert.deepStrictEqual(brainfuck(''), {
   memory: [],
@@ -174,41 +212,3 @@ assert.deepStrictEqual(brainfuck('>++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+
   output: 'Hello, World!',
   input: ''
 }, 'compiles "Hello, world!" program')
-
-function brainfuck (commands = '', { memory = [], loops = [], looping = false, innerLoops = 0, pointer = 0, output = '', input = '' } = {}) {
-  for (let commandIndex = 0; commandIndex < commands.length; commandIndex++) {
-    const command = commands[commandIndex]
-    if (looping) {
-      if (command === '[') innerLoops++
-      if (command === ']') {
-        if (innerLoops === 0) looping = false
-        else innerLoops--
-      }
-    }
-
-    if (command === '>' && pointer < 10) pointer++
-    if (command === '<' && pointer > 0) pointer--
-    if (command === '+') memory[pointer] ? memory[pointer]++ : memory[pointer] = 1
-    if (command === '-') memory[pointer] ? memory[pointer]-- : memory[pointer] = 0
-    if (command === '.') output += String.fromCharCode(memory[pointer])
-    if (command === ',' && input.length > 0) {
-      memory[pointer] = input.slice(0, 1).charCodeAt()
-      input = input.slice(1)
-    }
-    if (command === '[') {
-      if (memory[pointer] === 0) {
-        looping = true
-      } else {
-        loops.push(commandIndex)
-      }
-    }
-    if (command === ']') {
-      if (memory[pointer] === 0) {
-        loops.pop()
-      } else {
-        commandIndex = loops[loops.length - 1]
-      }
-    }
-  }
-  return { memory, loops, looping, pointer, output, input }
-}
