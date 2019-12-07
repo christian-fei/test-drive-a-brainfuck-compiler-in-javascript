f395ebea1035b249cc7565b21b65c7a4bafa5628
Christian Fei <crifei93@gmail.com>
2019-12-07 01:40:44 +0100

    introduce output property of brainfuck program

--- index.test.js
+++ index.test.js
@@ -6,50 +6,59 @@ const result = brainfuck('')
 assert.ok(result, 'result is defined')
 assert.deepStrictEqual(result, {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0
+  pointer: 0,
+  output: ''
 }, 'empty memory initialized with 10 bytes, pointer set to 0')
 
 assert.deepStrictEqual(brainfuck('>'), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 1
+  pointer: 1,
+  output: ''
 }, 'increment pointer')
 assert.deepStrictEqual(brainfuck('>>>>>>>>>>>'), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 10
+  pointer: 10,
+  output: ''
 }, 'do not increment pointer above 10')
 
 assert.deepStrictEqual(brainfuck('<', {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 1
+  pointer: 1,
+  output: ''
 }), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0
+  pointer: 0,
+  output: ''
 }, 'decrement pointer')
 
 assert.deepStrictEqual(brainfuck('<'), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0
+  pointer: 0,
+  output: ''
 }, 'do not decrement pointer below 0')
 
 assert.deepStrictEqual(brainfuck('+'), {
   memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0
+  pointer: 0,
+  output: ''
 }, 'increment byte at pointer')
 
 assert.deepStrictEqual(brainfuck('-', {
   memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0
+  pointer: 0,
+  output: ''
 }), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0
+  pointer: 0,
+  output: ''
 }, 'decrement byte at pointer')
 
-function brainfuck (commands = '', { pointer = 0, memory = Array(10).fill(0) } = {}) {
+function brainfuck (commands = '', { pointer = 0, memory = Array(10).fill(0) } = {}, output = '') {
   for (const command of commands) {
     if (command === '>' && pointer < 10) pointer++
     if (command === '<' && pointer > 0) pointer--
     if (command === '+') memory[pointer]++
     if (command === '-') memory[pointer]--
   }
-  return { memory, pointer }
+  return { memory, pointer, output }
 }
