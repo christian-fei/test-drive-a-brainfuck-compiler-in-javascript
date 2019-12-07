371d36634d83577177bb29b40a0c4f3836a0d77f
Christian Fei <crifei93@gmail.com>
2019-12-07 09:51:08 +0100

    pass testsG

--- index.test.js
+++ index.test.js
@@ -2,9 +2,8 @@
 
 const assert = require('assert')
 
-const result = brainfuck('')
-assert.ok(result, 'result is defined')
-assert.deepStrictEqual(result, {
+assert.ok(brainfuck(''), 'result is defined')
+assert.deepStrictEqual(brainfuck(''), {
   memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   pointer: 0,
   output: '',
@@ -74,25 +73,29 @@ assert.deepStrictEqual(brainfuck('.', {
   input: ''
 }, 'output the byte at the pointer')
 
-assert.deepStrictEqual(brainfuck('.', {
+assert.deepStrictEqual(brainfuck(',', {
+  memory: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  pointer: 0,
+  output: '',
+  input: '\u0001'
+}), {
   memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   pointer: 0,
   output: '',
   input: ''
-}), {
-  memory: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  pointer: 0,
-  output: '\u0001',
-  input: ''
-}, 'output the byte at the pointer incremented by 1')
+}, 'input a byte and store it in the byte at the pointer')
 
-function brainfuck (commands = '', { pointer = 0, memory = Array(10).fill(0) } = {}, output = '', input = '') {
+function brainfuck (commands = '', { pointer = 0, memory = Array(10).fill(0), output = '', input = '' } = {}) {
   for (const command of commands) {
     if (command === '>' && pointer < 10) pointer++
     if (command === '<' && pointer > 0) pointer--
     if (command === '+') memory[pointer]++
     if (command === '-') memory[pointer]--
     if (command === '.') output += String.fromCharCode(memory[pointer])
+    if (command === ',' && input.length > 0) {
+      memory[pointer] = input.slice(0, 1).charCodeAt()
+      input = input.slice(1)
+    }
   }
   return { memory, pointer, output, input }
 }
